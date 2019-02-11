f5c0dd7fd22df464b4875b310900c7a73b3243df
shekhar tyagi <shekhartyagi@virtualemployee.com>
2019-02-11 15:57:56 +0530

    first commit 	#

---
+++ models/User.js
@@ -0,0 +1,123 @@
+var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
+var secret = require('../config').secret;
+var _ = require('lodash');
+
+var UserSchema = new mongoose.Schema({
+  username: { type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true },
+  email: { type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true },
+  bio: String,
+  image: String,
+  favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Article' }],
+  following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  hash: String,
+  salt: String,
+  firstName: String,
+  lastName: String,
+  headline: String,
+  currentPosition: String,
+  country: String,
+  state: String,
+  industry: String
+}, { timestamps: true });
+
+UserSchema.plugin(uniqueValidator, { message: 'is already taken.' });
+
+UserSchema.methods.validPassword = function (password) {
+  var hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+  return this.hash === hash;
+};
+
+UserSchema.methods.setPassword = function (password) {
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+};
+
+UserSchema.methods.generateJWT = function () {
+  var today = new Date();
+  var exp = new Date(today);
+  exp.setDate(today.getDate() + 60);
+
+  return jwt.sign({
+    id: this._id,
+    username: this.username,
+    exp: parseInt(exp.getTime() / 1000),
+  }, secret);
+};
+
+UserSchema.methods.toAuthJSON = function () {
+  return {
+    username: this.username,
+    email: this.email,
+    token: this.generateJWT(),
+    bio: this.bio,
+    image: this.image,
+    firstName: this.firstName,
+    lastName: this.lastName,
+    headline: this.headline,
+    currentPosition: this.currentPosition,
+    country: this.country,
+    state: this.state,
+    industry: this.industry
+  };
+};
+
+UserSchema.methods.toProfileJSONFor = function (user) {
+  return {
+    username: this.username,
+    bio: this.bio,
+    image: this.image || 'https://static.productionready.io/images/smiley-cyrus.jpg',
+    following: user ? user.isFollowing(this._id) : false
+  };
+};
+
+UserSchema.methods.favorite = function (id) {
+  if (this.favorites.indexOf(id) === -1) {
+    this.favorites.push(id);
+  }
+  return this.save();
+};
+
+UserSchema.methods.unfavorite = function (id) {
+  this.favorites.remove(id);
+  return this.save();
+};
+
+UserSchema.methods.isFavorite = function (id) {
+  return this.favorites.some(function (favoriteId) {
+    return favoriteId.toString() === id.toString();
+  });
+};
+
+UserSchema.methods.follow = function (id) {
+  if (this.following.indexOf(id) === -1) {
+    this.following.push(id);
+  }
+
+  return this.save();
+};
+
+UserSchema.methods.unfollow = function (id) {
+  this.following.remove(id);
+  return this.save();
+};
+
+UserSchema.methods.isFollowing = function (id) {
+  return this.following.some(function (followId) {
+    return followId.toString() === id.toString();
+  });
+};
+
+UserSchema.methods.verifyData = (data = {}) => {
+  var result = {};
+  _.map(data, (val, key) => {
+    if (val && val.length) {
+      result[key] = val;
+    }
+  })
+  return result;
+}
+
+mongoose.model('User', UserSchema);
---
+++ routes/api/users.js
@@ -0,0 +1,70 @@
+var mongoose = require('mongoose');
+var router = require('express').Router();
+var passport = require('passport');
+var User = mongoose.model('User');
+var auth = require('../auth');
+
+router.get('/user', auth.required, function (req, res, next) {
+  User.findById(req.payload.id).then(function (user) {
+    if (!user) { return res.sendStatus(401); }
+
+    return res.json({ user: user.toAuthJSON() });
+  }).catch(next);
+});
+
+router.put('/user', auth.required, (req, res, next) => {
+  User.findById(req.payload.id)
+    .then((user) => {
+      if (!user) { return res.sendStatus(401); }
+      let { username, email, bio, image, password, firstName, lastName, headline, currentPosition, country, state, industry } = req.body.user;
+      // user = Object.assign(user, user.verifyData({ username, email, bio, image, password, firstName, lastName, headline, currentPosition, country, state, industry }))
+
+      if (username) user.username = username;
+      if (email) user.email = email;
+      if (bio) user.bio = bio;
+      if (image) user.image = image;
+      if (password) user.setPassword(password);
+      if (firstName) user.firstName = firstName;
+      if (lastName) user.lastName = lastName;
+      if (headline) user.headline = headline;
+      if (currentPosition) user.currentPosition = currentPosition;
+      if (country) user.country = country;
+      if (state) user.state = state;
+      if (industry) user.industry = industry;
+      return user.save().then(function () {
+        return res.json({ user: user.toAuthJSON() });
+      });
+    }).catch(next);
+});
+
+router.post('/users/login', (req, res, next) => {
+  let { email, password } = req.body.user;
+  if (!email) {
+    return res.status(422).json({ errors: { email: "can't be blank" } });
+  }
+  if (!password) {
+    return res.status(422).json({ errors: { password: "can't be blank" } });
+  }
+
+  passport.authenticate('local', { session: false }, function (err, user, info) {
+    if (err) { return next(err); }
+    if (user) {
+      return res.json({ user: user.toAuthJSON() });
+    } else {
+      return res.status(422).json(info);
+    }
+  })(req, res, next);
+});
+
+router.post('/users', (req, res, next) => {
+  var user = new User();
+  let { username, email, password } = req.body.user;
+  user.username = username;
+  user.email = email;
+  user.setPassword(password);
+  user.save().then(function () {
+    return res.json({ user: user.toAuthJSON() });
+  }).catch(next);
+});
+
+module.exports = router;
