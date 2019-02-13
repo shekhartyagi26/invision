f7e089a9e359d8269c2f2914370f8aa9bd3962fe
shekhar tyagi <shekhartyagi@virtualemployee.com>
2019-02-13 11:39:38 +0530

    comment 404 error in app.js

--- app.js
+++ app.js
@@ -51,12 +51,12 @@ require('./config/passport');
 
 app.use(require('./routes'));
 
-/// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// /// catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
 /// error handlers
 
